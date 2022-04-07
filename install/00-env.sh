c9ca51a0251553ce36a032a7c629925e75025e47
Sebastian Colomar <sebastian.colomar@gmail.com>
2022-04-07 02:08:22 +0200

    Update 00-env.sh

--- install/00-env.sh
+++ install/00-env.sh
@@ -1,6 +1,6 @@
-export ClusterName=openshift
+export ClusterName=okd
 export DomainName=sebastian-colomar.es 
 export master_type=t3a.xlarge
 export Publish=External
-export version=4.8.20
+export version=4.10.0-0.okd-2022-03-07-131213
 export worker_type=t3a.large
